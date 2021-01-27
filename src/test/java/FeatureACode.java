74d591561c6b00815e3e41631cc5ad9a92e26a96
Hanna <hannadilmac7@gmail.com>
2021-01-26 22:50:28 -0500

    movie on the phone 20 min

--- src/test/java/FeatureACode.java
+++ src/test/java/FeatureACode.java
@@ -3,6 +3,7 @@ public class FeatureACode {
         System.out.println("Awesome work for A started");
         System.out.println("21");
         //movie one the phone
+        //move one the phone 20 min
 
 
     }
